07fb341f755efe010f0172c4cbc822cf32507802
kust <kust@192.168.2.6>
2022-11-29 11:03:35 +0800

    [feature] axum-test 增加配置

--- axum-test/src/main.rs
+++ axum-test/src/main.rs
@@ -1,13 +1,23 @@
 use axum::{routing::get, Router};
-use std::net::SocketAddr;
+// use std::net::SocketAddr;
+use dotenv;
+
+mod common;
 mod api;
 
 #[tokio::main]
 async fn main() {
+    // 解析 .env 文件
+    // dotenv::dotenv().ok();
+    dotenv::from_path("axum-test/.env").ok();
+    // 初始化配置
+    let cfg = common::Config::from_env().expect("初始化配置失败");
+
     let app = Router::new()
         .route("/", get(api::range_randnum));
     
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = &cfg.web.addr.parse().unwrap();
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
 
     axum::Server::bind(&addr)
