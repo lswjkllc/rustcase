7cc9195b79371c62307fb50a47ba0edba80dacca
kust <kust@192.168.2.4>
2022-12-07 16:31:49 +0800

    [feature] 新增 lists -> second -> peek

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -23,6 +23,14 @@ impl<T> List<T> {
             head.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|head| &head.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|head| &mut head.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -74,5 +82,23 @@ mod test {
             list.push(i);
         }
         // drop(list);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
