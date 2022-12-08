f51ff09e2d72f9c01c5bc0e24866ffdd6f4de500
kust <kust@192.168.2.4>
2022-12-08 13:07:27 +0800

    [feature] 新增 lists -> fourth -> init+push_front+pop_front+drop

---
+++ lists/src/fourth.rs
@@ -0,0 +1,113 @@
+use std::rc::Rc;
+use std::cell::{RefCell};
+
+/// 双端链表定义
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self { head: None, tail: None }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_node = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_node.clone());
+                new_node.borrow_mut().next = Some(old_head);
+                self.head = Some(new_node);
+            },
+            None => {
+                self.tail = Some(new_node.clone());
+                self.head = Some(new_node);
+            },
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                },
+                None => {
+                    self.tail.take();
+                },
+            }
+
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+}
+
+/// Drop 实现
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+/// 双向节点定义
+pub struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(
+            Self{ elem: elem, next: None, prev: None },
+        ))
+    }
+}
+
+/// 指针定义
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn long_list() {
+        let mut list = List::new();
+        for i in 0..100000 {
+            list.push_front(i);
+        }
+        // drop(list);
+    }
+}
