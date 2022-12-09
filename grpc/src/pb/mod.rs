2daa9e7fd7f727b3ebbb9a5cb3af33589fb20c6d
kust <kust@192.168.2.4>
2022-12-09 13:31:18 +0800

    [feature] 转移 grpc ++

--- grpc/src/pb/mod.rs
+++ grpc/src/pb/mod.rs
@@ -28,3 +28,22 @@ impl PhoneNumber {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use prost::Message;
+    use super::person::{person::*, Person};
+
+    #[test]
+    fn basics() {
+        let phones = vec![PhoneNumber::new("111-222-3334", PhoneType::Mobile)];
+        let person = Person::new("Tyr Chen", 1, "tyr@awesome.com", phones);
+        let v1 = person.encode_to_vec();
+        let person1 = Person::decode(v1.as_ref()).unwrap();
+        assert_eq!(person, person1);
+
+        let json = serde_json::to_string_pretty(&person1).unwrap();
+
+        println!("{}", json);
+    }
+}
