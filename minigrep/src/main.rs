72d199e1c1692856baf7aa60c6d1976ea1fecf5f
kust <kust@192.168.2.4>
2022-12-21 22:22:04 +0800

    [feature] add minigrep command line tool

---
+++ minigrep/src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::build(&args).unwrap_or_else(|e| {
+        eprintln!("Problem parsing arguments: {e}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
